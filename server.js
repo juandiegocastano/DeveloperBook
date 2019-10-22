0dfcd788fdfeb8bffbb38b03aaf46f3a11098fd4
Juan Diego Castaño <j.castano@utp.edu.co>
2019-10-22 17:42:23 -0500

    2.1. Se agregan rutas con Express Router

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
+//Definir routes
+app.use("/api/users", require("./config/routes/api/users"));
+app.use("/api/profiles", require("./config/routes/api/profiles"));
+app.use("/api/auth", require("./config/routes/api/auth"));
+app.use("/api/posts", require("./config/routes/api/posts"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
